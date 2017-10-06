e48b73fda47c5353e16f6b519969ddfe752831e5
tinyherocarrot <andrw.huang@gmail.com>
2017-10-06 09:56:07 -0500

    first draft of front end

---
+++ js/logic2.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},400);
+		return false;
+	});
+	
+
+	// on "Search" click, smooth scroll to view 2
+	$("#submit-btn").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $(".view-2").offset().top
+	    }, 500);
+	});
+});
