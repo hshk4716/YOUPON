01304e2b9ccb21b0eb49634321f4beca6f42ec69
tinyherocarrot <andrw.huang@gmail.com>
2017-10-09 09:39:32 -0500

    added Brett's updates to logic.js 9:39am oct9, added a footer

--- logic.js
+++ logic.js
@@ -32,13 +32,13 @@ $("#submit-btn").click(function() {
             if(category_slug==="restaurants") {
 			    var imgUrl = response.deals[i].deal.image_url;
 			    var h1 = response.deals[i].deal.title;
-			    console.log(h1);
+			    // console.log(h1);
 			    var name = response.deals[i].deal.merchant.name;
 			    var nameUppercase = name.toUpperCase();
 			    var urlCorrected = encodeURIComponent(nameUppercase)
 			    var finalURL = urlCorrected.replace("'", "%27").replace("-", "%2D").replace("!", "%21");
 
-			    console.log("HUMERA LOOK HERE", finalURL);
+			    // console.log("HUMERA LOOK HERE", finalURL);
 			    var dealUrl = response.deals[i].deal.url;
 			    
 			    var newDealDiv = $("<div>");
@@ -58,10 +58,11 @@ $("#submit-btn").click(function() {
 				newDealDivBlock.append("<h5 class='card-title'>" + name + "</h5>");
 				newDealDivBlock.append("<p class='card-text text-muted'>" + h1 + "</p>");
 
-				var newDealDivInspection = $("<ul>");
-				newDealDivInspection.addClass("list-group list-group-flush");
-				var inspectionInfo = $("<li class='list-group-item'>" + "We've got inspection data" + "</li>");
-				newDealDivInspection.append(inspectionInfo);
+				var newDealDivInspection = $("<p>");
+				newDealDivInspection.addClass("inspection-ratings");
+				var inspectionInfo = $("<p>");
+				inspectionInfo.addClass("violation-data")
+				// newDealDivInspection.append(inspectionInfo);
 
 				var dealLinkBlock = $("<div>");
 				dealLinkBlock.addClass("card-block");
@@ -70,6 +71,7 @@ $("#submit-btn").click(function() {
 				newDealDivCard.append(newDealDivImg);
 				newDealDivCard.append(newDealDivBlock);
 				newDealDivCard.append(newDealDivInspection);
+				newDealDivCard.append(inspectionInfo);
 				newDealDivCard.append(dealLinkBlock);
 
 				newDealDiv.append(newDealDivCard);
@@ -84,11 +86,35 @@ $("#submit-btn").click(function() {
 				    data: {
 				      "$$app_token" : "YyemuyggIVumoVRnsbIlmLeqq"
 				    }
-				}).done(function(data) {
-				  // alert("Retrieved " + data.length + " records from the dataset!");
-				  console.log(data);
-				
-				}); // close city data API call
+				}).done(function(data) {			 
+			 		data.sort(function(a, b) {
+    		 		return parseFloat(b.inspection_date) - parseFloat(a.inspection_date);				
+					// console.log(data);
+					}); 
+				var akaName = (data[0].aka_name);
+			 	var riskResponse = data[0].risk;
+			 	var violationsResponse = data[0].violations;
+			 	var inspectionDate = data[0].inspection_date;
+			 	console.log(akaName);
+			 	console.log(inspectionDate);
+			 	console.log(riskResponse);
+			 	console.log(violationsResponse);
+
+				// for (var j = 0; j < data.length; j++) {
+					// var akaNameNew = (data[j].aka_name);
+					// var riskResponseNew = data[j].risk;
+					// var violationsResponseNew = data[j].violations;
+					// var inspectionDateNew = data[j].inspection_date;				
+										 	
+			 	// if (akaNameNew[j] === nameUppercase[i]) {
+			 		// $(".inspection-ratings").text(riskResponse);
+			 		// $(".violation-data").text(violationsResponse);
+			 	
+			 	// 	}	
+			 	// }
+			
+
+				}); // close city data API call (.done)
 
 			} // close if statement checking for restaurants	
 		} //close for loop
